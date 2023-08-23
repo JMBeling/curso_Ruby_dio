e1c016343bba8d5981ba78689fa194968142a304
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-08-23 18:57:17 -0300

    for

---
+++ Curso_3/for.rb
@@ -0,0 +1,9 @@
+#leia uma lista (array) de elementos - linguagens de programação
+
+linguagens = ['Ruby', 'Go', 'c++']
+count = 1
+for linguagem in linguagens
+  puts linguagem
+  puts count
+  count = count + 1
+end
