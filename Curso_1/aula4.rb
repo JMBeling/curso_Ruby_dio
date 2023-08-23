98c006183f9215e9f93e282de0a7f3e09900e657
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-08-23 06:38:59 -0300

    curso_2

---
+++ Curso_1/aula4.rb
@@ -0,0 +1,12 @@
+#Comentario
+# 1)Saida de dados na tela --> aparecer um comando na tela
+#Usuario digite seu nome
+print"Digite seu nome:    "
+
+#2)Programa permita que usuario preencha o que foi pedido
+nome = gets.chomp
+print "Digite seu sobrenome:   "
+sobrenome = gets.chomp
+
+# 3) Saida final
+puts "Oi #{nome} #{sobrenome}!"
