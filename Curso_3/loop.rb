c767169d7dd3391b0dd8c51d7a318c91cd771ad4
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-08-23 20:22:24 -0300

    while_time_loop

---
+++ Curso_3/loop.rb
@@ -0,0 +1,13 @@
+#Criar um contador
+
+count = 0
+
+loop do 
+  puts count
+
+
+  if count == 150
+      break
+      end
+  count += 1
+  end
---
+++ Curso_3/while.rb
@@ -0,0 +1,7 @@
+#comparar um número com uma condição
+num = 1
+
+while num < 100
+  puts num
+  num += 1
+end
