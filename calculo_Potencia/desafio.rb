44159c8d7f334414c623059a06ec44d9f006a182
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-09-03 13:31:51 -0300

    desafio

---
+++ calculo_Potencia/desafio.rb
@@ -0,0 +1,10 @@
+puts "Digite o primeiro numero: "
+num1 = gets.chomp.to_i
+
+puts "Digite o primeiro numero: "
+num2 = gets.chomp.to_i
+
+puts "Digite o primeiro numero: "
+num3 = gets.chomp.to_i
+#numeros = 
+puts "#{num1}, #{num2}, #{num3}"
