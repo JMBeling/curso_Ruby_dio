b831b12e4f0ca0c0467336daac4bfe434f3db162
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-09-03 11:01:56 -0300

    map

---
+++ Curso_4/map.rb
@@ -0,0 +1,18 @@
+numeros = [2,3,4,5]
+
+ novo_numeros = numeros.map do |x|
+      x * 5
+      end
+
+  puts "\n Array Original"
+  puts " #{numeros}"
+
+  puts "\n Novo Array"
+  puts " #{novo_numeros}"
+
+  numeros.map! do |x|
+    x * 5
+  end
+  puts "\n Array Original"
+  puts " #{numeros}"
+  puts " "
